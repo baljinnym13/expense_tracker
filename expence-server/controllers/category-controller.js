472d67a6bdd6a4240f746941d6ec50861da1b2a4
baljinnym13 <baljinnym1318@gmail.com>
2024-08-30 11:33:42 +0800

    front hiih shahaw

--- expence-server/controllers/category-controller.js
+++ expence-server/controllers/category-controller.js
@@ -6,7 +6,13 @@ const getAllCategory = async (req, res) => {
   res.status(200).json({ message: "success", user: data });
 };
 
-const createCategory = () => {};
+const createCategory = async (req, res) => {
+  const { name, description, category_image } = req.body;
+  const data =
+    await sql`INSERT INTO categories (name, description, category_image)
+   VALUES (  ${name}, ${description}, ${category_image})`;
+  res.status(201).json({ message: "success" });
+};
 const updateCategory = () => {};
 const deleteCategory = () => {};
 
